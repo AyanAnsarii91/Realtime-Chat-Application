76fea10aaf1ed009347a9a09a0638db42bb859b0
Ayan <ayancoder786@gmail.com>
2025-05-07 15:12:45 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
-import SignUpPage from "./pages/SignUpPage";
+// import HomePage from "./pages/HomePage";
+// import LoginPage from "./pages/LoginPage";
+// import SettingsPage from "./pages/SettingsPage";
+// import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
