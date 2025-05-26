eebcee579b6f73d6216d6de4b1b10bac0b19bf13
Ayan Ansari <ayancoder786@gnail.com>
2025-05-26 21:50:56 +0530

    fix this change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Skip certain warnings
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        // Use default for everything else
+        warn(warning)
+      }
+    }
+  }
 })
